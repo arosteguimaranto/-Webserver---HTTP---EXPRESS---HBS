dd95e94c4272a0c2f2758c08f7c8cb6ef28a712e
Arostegui Maranto <arosteguimaranto@outlook.com>
2022-11-07 12:01:56 -0600

    Finalizado, listo para subir

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require('express')
-const app = express()
-const port = 8080;
-
 var hbs = require('hbs');
+
+
+const app = express()
+const port = process.env.PORT;
+
+
 
 //Handlebars
 
