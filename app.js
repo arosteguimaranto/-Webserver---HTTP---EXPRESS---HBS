4a5d66e7c3ccebd1b739f610f1adc07557825a83
Arostegui Maranto <arosteguimaranto@outlook.com>
2022-11-07 11:35:00 -0600

    Usando Parciales con HBS

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+var hbs = require('hbs');
 
-//TODO: require('hbs');
+//Handlebars
+
 app.set('view engine', 'hbs');
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 //Servir contenido estatito
@@ -18,6 +20,24 @@ app.get('/', (req, res) => {
         titulo: 'Curso de Node'
     });
 });
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre:'Fernando Herrera',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre:'Fernando Herrera',
+        titulo: 'Curso de Node'
+    });
+});
+
+
+
+
 
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html')
